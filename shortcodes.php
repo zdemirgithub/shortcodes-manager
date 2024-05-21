0d92259ac6dd258dab40009ee827cacf2f802737
zdemirgithub <zehrademir.gm@gmail.com>
2024-05-21 20:28:28 +0300

    Create shortcodes.php

---
+++ shortcodes.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Custom Shortcodes Manager
+Description: Create and manage custom shortcodes for dynamic content.
+Version: 1.0
+*/
+
+// Register custom shortcode
+function custom_shortcode_function($atts) {
+    // Shortcode logic here
+}
+add_shortcode('custom_shortcode', 'custom_shortcode_function');
